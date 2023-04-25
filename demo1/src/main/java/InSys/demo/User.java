0b5ee406e43b0c9bea1639dcf56d70617226e9f3
sruju003 <80395231+sruju003@users.noreply.github.com>
2023-04-25 20:06:40 +0530

    Add files via upload

---
+++ demo1/src/main/java/InSys/demo/User.java
@@ -0,0 +1,21 @@
+package InSys.demo;
+
+import com.vaadin.flow.component.template.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @EqualsAndHashCode.Include
+    private int id;
+    private String name;
+    private String pno;
+    private String email;
+
+}
