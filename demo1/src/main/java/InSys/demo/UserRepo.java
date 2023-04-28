93ba7328982479ba7c7f868fee4b647780ae2c40
sruju003 <80395231+sruju003@users.noreply.github.com>
2023-04-28 13:32:24 +0530

    Delete demo1 directory

--- demo1/src/main/java/InSys/demo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package InSys.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends MongoRepository<User, Integer> {
-
-}
